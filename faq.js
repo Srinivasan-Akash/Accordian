c8a98b0d1daa9e6e74971e4495281d9d117ec673
Srinivasan-Akash <108281031+Srinivasan-Akash@users.noreply.github.com>
2022-10-16 19:40:36 +0530

    Increased Code Quality

--- faq.js
+++ faq.js
@@ -1,55 +1,70 @@
 const faqData = [
-  {
-    id: 1,
-    question: "Who are we?",
-    answer:
-      "We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.",
-  },
-  {
-    id: 2,
-    question: "What do we do?",
-    answer:
-      "Building learning communities with Our Affordable & Competent Courses",
-  },
-  {
-    id: 3,
-    question: "Are the community classes boring?",
-    answer: "No, not at all!!!",
-  },
+    {
+      id: 1,
+      question: "Who are we?",
+      answer:
+        "We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.",
+    },
+    {
+      id: 2,
+      question: "What do we do?",
+      answer:
+        "Building learning communities with Our Affordable & Competent Courses",
+    },
+    {
+      id: 3,
+      question: "Are the community classes boring?",
+      answer: "No, not at all!!!",
+    },
 ];
+  
+// Grabing the parent of all faq's
+const ACCORDIAN = document.querySelector(".accordian_body")
 
-let accordianBody;
-const faqs = [];
+// ACCORDIAN BODY
+    // FAQ
+      // FAQ HEADER
+        // H3 question
+        // SHOW BTN 
+      // FAQ BODY (ans)
 
-faqData.forEach((item) => {
-  let faq = document.createElement("div")
-  faq.classList.add("faq")
 
-  let showbtn = document.createElement("div")
-  showbtn.classList.add("show_btn")
-  showbtn.textContent = ""
 
-  let faqHeader = document.createElement("section")
-  faqHeader.classList.add("faq_header")
-  faqHeader.textContent = item.question
-  faqHeader.append(showbtn)
+faqData.forEach(item => {
+  // Creating all the elements
+  const FAQ = document.createElement("main") 
+  const FAQ_HEADER = document.createElement("section")
+  const FAQ_BODY = document.createElement("section")
+  const H3 = document.createElement("h3")
+  const SHOW_BTN = document.createElement("div")
 
-  let hiddenDesc = document.createElement("section")
-  hiddenDesc.classList.add("hidden")
-  hiddenDesc.textContent = item.answer
+  // Adding textContent
+  H3.textContent = item.question
+  FAQ_BODY.textContent = item.answer
 
-  faq.appendChild(faqHeader)
-  faq.appendChild(hiddenDesc)
 
-  document.querySelector(".accordian_body").appendChild(faq)
+  // Add Classes
+  FAQ.classList.add("faq")
+  FAQ_HEADER.classList.add("faq_header")
+  FAQ_BODY.classList.add("hidden")
+  SHOW_BTN.classList.add("show_btn")
 
+  // Apending All The Elements
+  FAQ_HEADER.appendChild(H3)
+  FAQ_HEADER.appendChild(SHOW_BTN)
+  FAQ.appendChild(FAQ_HEADER)
+  FAQ.appendChild(FAQ_BODY)
+  ACCORDIAN.appendChild(FAQ)
+
+
+  // Adding Event Listener 
   let isOpen = true
-  showbtn.addEventListener("click", () => {
+  SHOW_BTN.addEventListener("click", () => {
       if(isOpen){
-        hiddenDesc.style.display = "block"
+        FAQ_BODY.style.display = "block"
         isOpen = false        
       } else {
-        hiddenDesc.style.display = "none"
+        FAQ_BODY.style.display = "none"
         isOpen = true
       }
   })
